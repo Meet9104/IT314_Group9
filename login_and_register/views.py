6de56406927bb48b38de8471b5e50bce4f2a2b9d
Maharth <mrthakar20012003@gmail.com>
2023-03-18 16:33:59 +0530

    Implemented login and register functionalities (both frontend and backend) for our project

---
+++ login_and_register/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect
+from .forms import SignUpForm, LoginForm
+from django.contrib.auth import authenticate, login
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def register(request):
+    msg = None
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            msg = 'user created'
+            return redirect('login_view')
+        else:
+            msg = 'form is not valid'
+    else:
+        form = SignUpForm()
+    return render(request,'register.html', {'form': form, 'msg': msg})
+
+
+def login_view(request):
+    form = LoginForm(request.POST or None)
+    msg = None
+    if request.method == 'POST':
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None and user.is_faculty:
+                login(request, user)
+                return redirect('facultypage')
+            elif user is not None and user.is_ta:
+                login(request, user)
+                return redirect('ta')
+            elif user is not None and user.is_student:
+                login(request, user)
+                return redirect('student')
+            else:
+                msg= 'invalid credentials'
+        else:
+            msg = 'error validating form'
+    return render(request, 'login.html', {'form': form, 'msg': msg})
+
+
+def faculty(request):
+    return render(request,'faculty.html')
+
+
+def ta(request):
+    return render(request,'ta.html')
+
+
+def student(request):
+    return render(request,'student.html')
