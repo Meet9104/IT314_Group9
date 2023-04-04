64e2fd50df9e91ed1ee2834593a9cafffc3aafbf
Maharth <mrthakar20012003@gmail.com>
2023-04-04 21:59:10 +0530

    implemented mongo login system with updated login ui

--- login_and_register/views.py
+++ login_and_register/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from .forms import SignUpForm, LoginForm
 from django.contrib.auth import authenticate, login
 # Create your views here.
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://maharth:maharth@cluster0.xsqej9v.mongodb.net/test')
+db = client['Leave_Management_System']
 
 
 def index(request):
@@ -30,30 +33,55 @@ def login_view(request):
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None and user.is_faculty:
-                login(request, user)
-                return redirect('facultypage')
-            elif user is not None and user.is_ta:
-                login(request, user)
-                return redirect('ta')
-            elif user is not None and user.is_student:
-                login(request, user)
-                return redirect('student')
-            else:
-                msg= 'invalid credentials'
-        else:
-            msg = 'error validating form'
+            # print(username, password)
+
+            studenttable = db['student info']
+            reply = studenttable.find_one({'email': username})
+            # print(reply)
+            if reply:
+                if reply['password'] == password:
+                    return render(request, 'student.html', {'user' : reply})
+                
+            facultytable = db['faculty info']
+            reply = facultytable.find_one({'email': username})
+            # print(reply)
+            if reply:
+                if reply['password'] == password:
+                    return render(request, 'faculty.html', {'user' : reply})
+                
+            tatble = db['ta info']
+            reply = tatble.find_one({'email': username})
+            # print(reply)
+            if reply:
+                if reply['password'] == password:
+                    return render(request, 'ta.html', {'user' : reply})
     return render(request, 'login.html', {'form': form, 'msg': msg})
 
+            # return redirect('student.html')
+            # print(reply)
+        #     user = authenticate(username=username, password=password)
+        #     if user is not None and user.is_faculty:
+        #         login(request, user)
+        #         return redirect('facultypage')
+        #     elif user is not None and user.is_ta:
+        #         login(request, user)
+        #         return redirect('ta')
+        #     elif user is not None and user.is_student:
+        #         login(request, user)
+        #         return redirect('student')
+        #     else:
+        #         msg= 'invalid credentials'
+        # else:
+        #     msg = 'error validating form'
+  
 
-def faculty(request):
-    return render(request,'faculty.html')
+# def faculty(request):
+#     return render(request,'faculty.html')
 
 
-def ta(request):
-    return render(request,'ta.html')
+# def ta(request):
+#     return render(request,'ta.html')
 
 
-def student(request):
-    return render(request,'student.html')
+# def student(request):
+#     return render(request,'student.html')
