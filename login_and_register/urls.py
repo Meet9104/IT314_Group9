ea24dadcc93476b64fb1dcf41d26e31a74a8bc68
Meet9104 <meet6321@gmail.com>
2023-04-05 13:45:45 +0530

    Final version of mid evaluation

--- login_and_register/urls.py
+++ login_and_register/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name= 'index'),
     path('login/', views.login_view, name='login_view'),
     path('register/', views.register, name='register'),
-    path('facultypage/', views.faculty, name='facultypage'),
-    path('ta/', views.ta, name='ta'),
-    path('student/', views.student, name='student'),
+    # path('facultypage/', views.faculty, name='facultypage'),
+    # path('ta/', views.ta, name='ta'),
+    # path('student/', views.student, name='student'),
 ]
